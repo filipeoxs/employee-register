716fc9d16366cbb087a03bfa7cd5908eb39b7a56
Filipe <filipeoxs@gmail.com>
2022-07-19 15:43:07 -0300

    Configurando o Crispy no formulário

--- employee_register/admin.py
+++ employee_register/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Employee, Position
 
 # Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    fields = ['fullname','emp_code','mobile','position']
+admin.site.register(Employee, EmployeeAdmin)
+
+class PositionAdmin(admin.ModelAdmin):
+    fields = ['title']
+admin.site.register(Position, PositionAdmin)
--- employee_register/views.py
+++ employee_register/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from employee_register.form import EmployeeForm
@@ -7,7 +8,13 @@ def employee_list(request):
     return render(request,'employee_register/employee_list.html')
 
 def employee_form(request):
-    form = EmployeeForm()
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            employee = form.save()
+            employee.save()
+    else:
+        form = EmployeeForm()
     return render(request,'employee_register/employee_form.html',{'form':form})
 
 def employee_delete(request):
