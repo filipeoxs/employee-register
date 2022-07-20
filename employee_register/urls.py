3e2f4c724aea74749d298b80f2f436a1c59a921e
Filipe <filipeoxs@gmail.com>
2022-07-20 11:05:58 -0300

    Campo de pesquisa configurado

--- employee_register/urls.py
+++ employee_register/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.employee_list, name='employee_list'), #List
     path('create/',views.employee_form, name='employee_form'), #Create
-    path('<int:id>/',views.employee_update, name='employee_edit'), #Update
+    path('update/<int:id>/',views.employee_update, name='employee_edit'), #Update
     path('delete/<int:id>/',views.employee_delete, name='employee_delete'), #Delete
+    path('search/',views.search_employee, name='employee_search') #Search
 ]
--- employee_register/views.py
+++ employee_register/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Employee
 from employee_register.form import EmployeeForm
 
 # Create your views here.
 def employee_list(request):
-    return render(request,'employee_register/employee_list.html',{'employee_list':Employee.objects.all()})
+    return render(request,'employee_register/employee_list.html',{'employee_list':Employee.objects.all().order_by('fullname')})
 
 def employee_form(request):
     if request.method == 'POST':
@@ -30,3 +30,13 @@ def employee_delete(request,id):
     obj = get_object_or_404(Employee, id= id)
     obj.delete()
     return redirect('/')
+
+def search_employee(request):
+    search_user = request.GET.get('username')
+    #print(search_user)
+    if search_user:
+        user = Employee.objects.filter(Q(fullname__icontains = search_user))
+    else:
+        
+        user = Employee.objects.all()
+    return render(request,'employee_register/employee_list.html',{'employee_list':user})
