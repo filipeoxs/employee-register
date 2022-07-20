b02dfdb5754589fbc12af4e3b2a9b8fd7baf3aa0
Filipe <filipeoxs@gmail.com>
2022-07-20 08:58:03 -0300

    Crud finalizado

--- employee_register/urls.py
+++ employee_register/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.employee_list, name='employee_list'), #List
     path('create/',views.employee_form, name='employee_form'), #Create
-    path('<int:id>/',views.employee_form, name='employee_edit'), #Update
-    path('delete/',views.employee_delete, name='delete'), #Delete
+    path('<int:id>/',views.employee_update, name='employee_edit'), #Update
+    path('delete/<int:id>/',views.employee_delete, name='employee_delete'), #Delete
 ]
--- employee_register/views.py
+++ employee_register/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Employee
 from employee_register.form import EmployeeForm
 
@@ -6,7 +7,7 @@ from employee_register.form import EmployeeForm
 def employee_list(request):
     return render(request,'employee_register/employee_list.html',{'employee_list':Employee.objects.all()})
 
-def employee_form(request,id=0):
+def employee_form(request):
     if request.method == 'POST':
         form = EmployeeForm(request.POST)
         if form.is_valid():
@@ -14,12 +15,18 @@ def employee_form(request,id=0):
             employee.save()
             return redirect('/')
     else:
-        if id == 0:
-            form = EmployeeForm()
-        else:
-            employee = Employee.objects.get(pk = id)
-            form = EmployeeForm(instance=employee)
+        form = EmployeeForm()
     return render(request,'employee_register/employee_form.html',{'form':form})
 
-def employee_delete(request):
-    return render(request,'employee_register/employee_delete.html')
+def employee_update(request,id):
+    obj = get_object_or_404(Employee, id = id)
+    form = EmployeeForm(request.POST or None, instance= obj)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request,'employee_register/employee_form.html',{'form':form})
+
+def employee_delete(request,id):
+    obj = get_object_or_404(Employee, id= id)
+    obj.delete()
+    return redirect('/')
